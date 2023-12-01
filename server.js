5aa163376fb8676e09bf91d046b83d7ad7d94e6d
unknown <jozelinesuarez@gmail.com>
2023-12-01 14:31:00 -0600

    Added uuid npm package, changes to routes for heroku

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('./helpers/fsUtils');
+
 const db = require('./db/db.json');
-const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -30,60 +35,36 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.post('/api/notes', async (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
-
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuid()
-        };
-
-        try {
-            const data = await fs.readFile('./db/db.json', 'utf8');
-            const parsedNotes = JSON.parse(data);
-
-            parsedNotes.push(newNote);
-
-            await fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 2), 'utf8');
-
-            const response = {
-                status: 'success',
-                body: newNote,
-            };
-
-            console.log(response);
-            res.status(201).json(response);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json('Internal Server Error');
-        }
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
     } else {
-        res.status(500).json('Error in posting note');
+      res.error('Error in adding note');
     }
 });
 
-
-app.delete('/api/notes/:id', async (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
-
-    try {
-        const data = await fs.readFile('./db/db.json', 'utf8');
-        const json = JSON.parse(data);
-
-        const result = json.filter((note) => note.id.toString() !== noteId);
-
-        await fs.writeFile('./db/db.json', JSON.stringify(result, null, 2), 'utf8');
-
-        res.json(`Note ${noteId} has been deleted`);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json('Internal Server Error');
-    }
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id !== noteId);
+  
+        writeToFile('./db/db.json', result);
+  
+        res.json(`Item ${noteId} has been deleted`);
+      });
 });
-
+  
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
 );
